6e11454bebe223cf9a0484bc3cfae7f085f03107
Anne Estevam <helivianne@gmail.com>
2022-11-29 16:36:06 -0300

    atualizacao do index.js
    Foi atualizado o nome do arquivo

--- inputs/serie-do-dia/retorno-serie.js
+++ inputs/serie-do-dia/retorno-serie.js
@@ -1,4 +1,4 @@
-let serie = ['Smallville', 'Wandavision', 'Loki', 'Gothan', 'Arrow', 'Flash', 'DC Legends'];
+let serie = ['Smallville', 'Wandavision', 'Loki', 'Wednesday', 'Arrow', 'Flash', 'DC Legends'];
 
 function retornaSerieDia(indexSerie) {
     return serie[indexSerie];
